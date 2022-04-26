4d7a673f6ca0a986177be540b20c609b917fb5da
Kiran Manam <manamkiranyadav@gmail.com>
2022-04-26 17:37:19 +0530

    Added Welcome Controller

--- src/main/java/kiran/manam/controllers/WelcomeController.java
+++ src/main/java/kiran/manam/controllers/WelcomeController.java
@@ -1,5 +1,13 @@
 package kiran.manam.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class WelcomeController {
 
+	@GetMapping("/welcome")
+	public String sayWelcome() {
+		return "Hello Welcome to my site";
+	}
 }
