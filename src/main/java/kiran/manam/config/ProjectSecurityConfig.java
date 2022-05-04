937a8b4d5d9bbdc012e68e59d667a8688187313d
Kiran Manam <manamkiranyadav@gmail.com>
2022-05-04 14:56:30 +0530

    Added code to set project specific endpoint security rules

--- src/main/java/kiran/manam/config/ProjectSecurityConfig.java
+++ src/main/java/kiran/manam/config/ProjectSecurityConfig.java
@@ -5,11 +5,13 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter{
-	
+public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests((requests) -> requests.anyRequest().authenticated());
+		http.authorizeRequests().antMatchers("/myAccount").authenticated().antMatchers("/myBalance").authenticated()
+				.antMatchers("/myLoans").authenticated().antMatchers("/myCards").authenticated()
+				.antMatchers("/notices").permitAll().antMatchers("contact").permitAll();
 		http.formLogin();
 		http.httpBasic();
 	}
