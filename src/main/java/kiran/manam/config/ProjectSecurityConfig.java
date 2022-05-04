52156f9419764ebe1ae408df35339cbd54b6a020
Kiran Manam <manamkiranyadav@gmail.com>
2022-05-04 14:45:57 +0530

    Added default Project Security Config

---
+++ src/main/java/kiran/manam/config/ProjectSecurityConfig.java
@@ -0,0 +1,17 @@
+package kiran.manam.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests((requests) -> requests.anyRequest().authenticated());
+		http.formLogin();
+		http.httpBasic();
+	}
+
+}
