33e8c48361fb7c3341cfc121865474d5ee4ba909
Kiran Manam <manamkiranyadav@gmail.com>
2022-05-09 14:37:17 +0530

    Creating Users via InMemoryUserDetailsManager

--- src/main/java/kiran/manam/config/ProjectSecurityConfig.java
+++ src/main/java/kiran/manam/config/ProjectSecurityConfig.java
@@ -4,8 +4,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -19,10 +22,23 @@ public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
 		http.httpBasic();
 	}
 
+	/*
+	 * @Override protected void configure(AuthenticationManagerBuilder auth) throws
+	 * Exception {
+	 * auth.inMemoryAuthentication().withUser("admin").password("admin").authorities
+	 * ("admin").and().withUser("read")
+	 * .password("read").authorities("read").and().passwordEncoder(
+	 * NoOpPasswordEncoder.getInstance()); }
+	 */
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("admin").password("admin").authorities("admin").and().withUser("read")
-				.password("read").authorities("read").and().passwordEncoder(NoOpPasswordEncoder.getInstance());
+		InMemoryUserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
+		UserDetails user1 = User.withUsername("admin").password("admin").authorities("admin").build();
+		UserDetails user2 = User.withUsername("read").password("read").authorities("read").build();
+		userDetailsService.createUser(user1);
+		userDetailsService.createUser(user2);
+		auth.userDetailsService(userDetailsService);
 	}
 
 }
