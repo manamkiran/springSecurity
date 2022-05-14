457ecdf1ac92fed1ec502c4505cb0358005d4b25
Kiran Manam <manamkiranyadav@gmail.com>
2022-05-14 22:35:52 +0530

    Adding BCryptPasswordEncoder

--- src/main/java/kiran/manam/config/ProjectSecurityConfig.java
+++ src/main/java/kiran/manam/config/ProjectSecurityConfig.java
@@ -1,15 +1,15 @@
 package kiran.manam.config;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
 @Configuration
 public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -32,19 +32,26 @@ public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
 	 * NoOpPasswordEncoder.getInstance()); }
 	 */
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		InMemoryUserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
-		UserDetails user1 = User.withUsername("admin").password("admin").authorities("admin").build();
-		UserDetails user2 = User.withUsername("read").password("read").authorities("read").build();
-		userDetailsService.createUser(user1);
-		userDetailsService.createUser(user2);
-		auth.userDetailsService(userDetailsService);
+	/*
+	 * @Override protected void configure(AuthenticationManagerBuilder auth) throws
+	 * Exception { InMemoryUserDetailsManager userDetailsService = new
+	 * InMemoryUserDetailsManager(); UserDetails user1 =
+	 * User.withUsername("admin").password("admin").authorities("admin").build();
+	 * UserDetails user2 =
+	 * User.withUsername("read").password("read").authorities("read").build();
+	 * userDetailsService.createUser(user1); userDetailsService.createUser(user2);
+	 * auth.userDetailsService(userDetailsService); }
+	 */
+	
+	@Bean
+	public UserDetailsService userDetailsService(DataSource datasource) {
+		return new JdbcUserDetailsManager(datasource);
 	}
 	
 	@Bean
 	public PasswordEncoder passwordEncoder(){
-		return NoOpPasswordEncoder.getInstance();
+		//return NoOpPasswordEncoder.getInstance();
+		return new BCryptPasswordEncoder();
 	}
 
 }
